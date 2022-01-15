e7a9d3b1a658e00e7bf70c9df861eec8ba6e41e7
Rob Cannon <rpcann@gmail.com>
2022-01-15 14:38:40 -0500

    Some basic feedback for cli errors

--- src/cli.ts
+++ src/cli.ts
@@ -30,9 +30,40 @@ if (process.env.GIT_SEMVER_DEBUG) {
 const logger = LoggerFactory.get("git-semver-cli");
 
 const main = async () => {
+  if (!GITHUB_TOKEN) {
+    logger.error("Missing required 'GITHUB_TOKEN' in environment variables");
+    process.exit(1);
+  }
+
+  if (process.argv.length < 4) {
+    logger.error(
+      "Missing required arguments, expected two arguments for 'repository' and 'reference'"
+    );
+    process.exit(1);
+  }
+
   const [repository, reference] = process.argv.slice(2);
 
   const [owner, name] = repository.split("/");
+
+  if (!owner) {
+    logger.error(
+      `Invalid github repository owner '${owner}' from repository argument '${repository}'`
+    );
+    process.exit(1);
+  }
+
+  if (!name) {
+    logger.error(
+      `Invalid github repository name '${name}' from repository argument '${repository}'`
+    );
+    process.exit(1);
+  }
+
+  if (!reference) {
+    logger.error(`Invalid github reference '${reference}'`);
+    process.exit(1);
+  }
 
   logger.info(`Owner: ${owner}`);
   logger.info(`Name: ${name}`);
